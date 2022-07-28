1b9c6ed9816194d5dc08027b34898e984a49c420
MateoRodriguez1426 <mrodrguez25@misena.edu.co>
2022-07-28 11:33:18 -0500

    Se realizo el manejo y escritura de datos en archivo de txt de los datos de usuario

--- Ejercicio Nomina.py
+++ Ejercicio Nomina.py
@@ -1,8 +1,12 @@
 import re
+from io import open
 print("Cordial saludo empleado")
 
 document = input("Digite su numero de identificacion \n")
-
+save = open("Guardado.txt", "w")
+save.write("Numero de Documento")
+save.write(document)
+save.close()
 
 if document.isdigit() == True and len(document) == 10:
     print("Se ha registrado correctamente el Numero de Documento \n")
@@ -15,6 +19,10 @@ else:
 
 
 Name = str(input("Digite su primer nombre \n"))
+save2 = open("Guardado.txt", "w")
+save2.write("Primer Nombre")
+save2.write(Name)
+save2.close()
 vef = re.findall(r'[a-zA-Z]{2,20}', Name)
 if vef:
     print("Se ha registrado correctamente su nombre \n")
@@ -27,6 +35,11 @@ else:
             print("Se ha registrado correctamente su nombre \n")
 
 sName = str(input("Digite su segundo nombre (Si no tiene digite NA)\n"))
+save3 = open("Guardado.txt", "w")
+save3.write("Segundo Nombre")
+save3.write(sName)
+save3.close()
+
 vef1 = re.findall(r'[a-zA-Z]{2,20}', sName)
 
 if vef1:
@@ -41,6 +54,10 @@ else:
 
 
 lName = str(input("Digite sus Apellidos completos \n"))
+save4 = open("Guardado.txt", "w")
+save4.write("Apellidos Completos")
+save4.write(lName)
+save4.close()
 
 vef2 = re.findall(r'[a-zA-Z]{2,40}\s', lName)
 if vef2:
@@ -55,6 +72,10 @@ else:
 
 
 wage = str(input("Digite su salario base \n"))
+save5 = open("Guardado.txt", "w")
+save5.write("Salario Base")
+save5.write(wage)
+save5.close()
 
 vef3 = re.findall('^\d[0-9]{6,8}',wage)
 if vef3:
@@ -68,6 +89,11 @@ else:
             print("Se ha registrado correctamente su salario \n")
 
 workedDays = int(input("Digite el numero de dias laborados\n"))
+workedDaysT = str(workedDays)
+save6 = open("Guardado.txt", "w")
+save6.write("Dias Trabajados")
+save6.write(workedDaysT)
+save6.close()
 
 if workedDays >= 10 or workedDays <= 31:
     workedDaysT = str(workedDays)
@@ -92,35 +118,26 @@ else:
         vef4 = re.findall('^\d[0-9]{2}', workedDaysT)
         if vef4:
             print("Se han registrado correctamente sus Dias Trabajados\n")
-
-
-
-'''
-
-
-
-
-
-
+wageT = int(wage)
 transporte = 0
-if (SalarioBase <= 2000000):
+if (wageT <= 2000000):
     transporte = 117100
 
-SalarioNeto = int((SalarioBase/30)*DT)
+SalarioNeto = int((wageT/30)*workedDays)
 pension = int((SalarioNeto*0.04))
 salud = int((SalarioNeto*0.04))
 Nomina = int(SalarioNeto-(pension+salud))
-transporte = int(transporte/30)*DT
+transporte = int(transporte/30)*workedDays
 Nomina = int(Nomina+transporte)
 
 
-YN = input("sun datos son " + Nombre + documento + " Confirme escribiendo un YES de lo contrario escriba NO \n")
+YN = input("Sus datos son " + Name + document + " Confirme escribiendo un YES de lo contrario escriba NO \n")
 
 if (YN == "YES"):
     print("Su descuento por pension es:", pension)
     print("Su descuento por salud es:", salud)
     print("Su subsidio por transorte es de",transporte)
-    print("Su salario neto por trabajar", DT, "dias, es de ", Nomina)
+    print("Su salario neto por trabajar", workedDays, "dias, es de ", Nomina)
 while (YN !="YES"):
-    YN = input("sun datos son " + Nombre + documento + "Confirme escribiendo un YES de lo contrario escriba NO \n")
-'''
+    YN = input("Sus datos son " + Name + lName + document + "Confirme escribiendo un YES de lo contrario escriba NO \n")
+
